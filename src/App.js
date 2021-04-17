60a3442865cc496260cf6eff8fc90ce7a681dbd2
Joan Currie <jecurrie8@gmail.com>
2021-04-16 22:36:56 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom"; //route is a component:= define diff paths in the url.
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout"; //
 
 function App() {
+  // eg: localhost:3000/favorites
+  //my-page.com
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn Redux" />
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
-
 export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,25 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-8c794-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add Meetup Page</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
